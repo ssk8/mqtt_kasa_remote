80ccc70980cb784b8f236cd7d4dea986f9046fc5
ssk8 <stinkyskate@pm.me>
2026-07-14 15:16:15 -0500

    switched to ips

--- remote.py
+++ remote.py
@@ -1,19 +1,16 @@
 import paho.mqtt.client as mqtt
 import asyncio
-from kasa import Discover
+import kasa
 import json
 
 MQTT_HOST = "tbox"
-IP_NUMS={"two":"192.168.1.138",
-	"one":"192.168.1.199",
-	"four":"192.168.1.101"}
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected: {reason_code}")
     client.subscribe("DroidPad/Events")
 
 async def turn(dev_num, next_state):
-    dev = await Discover.discover_single(IP_NUMS[dev_num])
+    dev = await kasa.Discover.discover_single(f'192.168.1.{dev_num}')
     if next_state: await dev.turn_on()
     else: await dev.turn_off()
     await dev.update()
@@ -21,7 +18,10 @@ async def turn(dev_num, next_state):
 def on_message(client, userdata, msg):
     print(f' {msg.topic:^27} {msg.payload}')
     pl = json.loads(msg.payload.decode('utf8'))
-    asyncio.run(turn(pl["id"], pl["state"]))
+    try:
+        asyncio.run(turn(pl["id"], pl["state"]))
+    except kasa.exceptions.TimeoutError:
+        print("Timed out!")
 
 def main():
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
